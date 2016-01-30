5774e1d53bbb082c22a9cafa71bb36652187fd34
46066294 <46066294p@iespoblenou.org>
2016-01-30 18:44:14 +0100

    INESTABLE

--- src/ScannerPort_main.java
+++ src/ScannerPort_main.java
@@ -1,7 +1,28 @@
+import java.net.Socket;
+
 /**
  * Created by Mat on 30/01/2016.
  */
 
+public class ScannerPort_main {
+    public static void main(String[] args) {
+        Socket socket;
+        String ip = "localhost";
+
+        for(int port = 0; port <= 13000; port++){
+            try{
+                socket = new Socket(ip, port);
+
+                System.out.println("Puerto " + port + " Abierto");
+                socket.close();
+            }
+            catch(Exception ex){
+                //ex.printStackTrace();
+            }
+        }
+    }
+}
+/*
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,20 +32,20 @@ public class ScannerPort_main {
     public static void main(String[]args){
         ExecutorService executor = Executors.newCachedThreadPool();
 
-        int ini = 1,fin = 100;
-        while(fin <= 65500){
+        int ini = 0, fin = 65536;
+        while(ini <= 65536){
             executor.execute(new Run(ini,fin));
-            fin += 100;
-            ini += 100;
+            fin++;
+            ini++;
         }
-        executor.execute(new Run(65501,65535));
+        //executor.execute(new Run(65501,65535));//65536
 
     }
 
 }
 
 class Run implements Runnable{
-    int puerto = 0;
+    int puerto;
     int fin;
     public Run(int port,int fin){
         puerto = port;
@@ -32,14 +53,14 @@ class Run implements Runnable{
     }
 
     public void run(){
-        Scanner Scanner = new Scanner(puerto, fin);
+        ScannerPort scanner = new ScannerPort(puerto, fin);
     }
 }
 
 
-class Scanner {
+class ScannerPort {
 
-    public Scanner(int puerto, int fin){
+    public ScannerPort(int puerto, int fin){
 
         Socket socket;
         String ip = "localhost";
@@ -52,9 +73,10 @@ class Scanner {
                 socket.close();
             }
             catch(Exception ex){
-
+                //ex.printStackTrace();
             }
 
         }
     }
 }
+*/
